125f013ad4c6a682525837ad5f6ac05772e4ffe2
Dave Fyre <idevrath@gmail.com>
2017-07-21 12:16:12 -0700

    Commit

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    Pname = models.CharField(max_length=128)
+    Cname = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return self.Pname
