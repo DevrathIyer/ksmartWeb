3a3e52f5c0d2eae8720ae32604d53c2353dc2742
Dave Fyre <idevrath@gmail.com>
2017-07-21 14:17:30 -0700

    Commit

--- tracker/models.py
+++ tracker/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    Pname = models.CharField(max_length=128)
-    Cname = models.CharField(max_length=128)
-    UID = models.CharField(max_length=128)
-    PicUrl = models.URLField()
-    PhoneNumber = models.PositiveIntegerField()
+    Pname = models.CharField(max_length=128, default = "Test")
+    Cname = models.CharField(max_length=128, default = "Test")
+    UID = models.CharField(max_length=128, default = "Test")
+    PicUrl = models.URLField(default = "http://ksmart.herokuapp.com")
+    PhoneNumber = models.PositiveIntegerField(default = 0)
 
     def __str__(self):
         return self.UID
